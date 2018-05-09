0608f13c03832405c64f2b29903216d92b3ed666
Aries Camitan <ariescamitan@yahoo.com>
2018-05-09 20:26:37 +0800

    update the home page

--- PubSubChat/Pages/Chat.cshtml.cs
+++ PubSubChat/Pages/Chat.cshtml.cs
@@ -6,8 +6,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace PubSubChat.Namespace
+namespace PubSubChat.Pages
 {
+    [Authorize]
     public class ChatModel : PageModel
     {
         public void OnGet()
